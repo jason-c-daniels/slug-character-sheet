b543525c31fb5d66cc4bd9b45ea2666d67d1c419
Jason Daniels <jason.c.daniels@gmail.com>
2020-09-04 20:46:32 -0500

    * tweaked application title to match repo * published latest build to docs

--- src/model/viewOptions.js
+++
@@ -1,8 +0,0 @@
-export default class ViewOptions {
-    printBothPages=true;
-    saveAlsoDownloads=true;
-    constructor(printBothPages=true,saveAlsoDownloads=true){
-        this.printBothPages=printBothPages;
-        this.saveAlsoDownloads=saveAlsoDownloads;
-    }
-}
